c65add96ee3850c8995d7e931f8df5847944b8d2
ch3rrywolf <ch3rrywolf111@gmail.com>
2024-07-06 23:38:01 +0100

    Review Crud controller, model, router, testing ..

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -17,3 +17,15 @@ const addReview = async (req, res) => {
     const review = await Review.create(data)
     res.status(200).send(review)
 }
+
+//2. Get All Reviews
+
+const getAllReviews = async (req, res) => {
+    const reviews = await Review.finAll({})
+    res.status(200).send(reviews)
+}
+
+module.exports = {
+    addReview,
+    getAllReviews
+}
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,10 +1,20 @@
+// import controller review, products
 const productController = require('../controllers/productController.js')
+const reviewController = require('../controllers/reviewController.js')
 
+// router
 const router = require('express').Router()
 
+//use routers
 router.post('/addProduct', productController.addProduct)
 router.get('/allProducts', productController.getAllProducts)
 router.get('/published', productController.getPublishedProduct)
+
+// Reviews Url and Controller
+router.post('/addReview', reviewController.addReview)
+router.get('/allReviews', reviewController.getAllReviews)
+
+// Products router
 router.get('/:id', productController.getOneProduct)
 router.put('/:id', productController.updateProduct)
 router.delete('/:id', productController.deleteProduct)
